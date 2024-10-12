5cd06d27193bfc35a5ae2204d8920f86d555ee9b
Fernando Valdez <fernando.valdez@io.builders>
2024-10-12 15:58:37 +0200

    new changes

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.querySelector('[aria-controls="mobile-menu"]');
     const mobileMenu = document.getElementById('mobile-menu');
-    
+
     menuButton.addEventListener('click', () => {
-      const isOpen = menuButton.getAttribute('aria-expanded') === 'true';
-      menuButton.setAttribute('aria-expanded', !isOpen);
-      mobileMenu.classList.toggle('hidden');
+        const isOpen = menuButton.getAttribute('aria-expanded') === 'true';
+        menuButton.setAttribute('aria-expanded', !isOpen);
+        mobileMenu.classList.toggle('hidden');
     });
-  
-    const userMenuButton = document.getElementById('user-menu-button');
-    const userMenu = document.querySelector('[role="menu"]');
-  
-    userMenuButton.addEventListener('click', () => {
-      userMenu.classList.toggle('hidden');
+
+    const servicesMobileButton = document.getElementById('services-mobile');
+    const mobileServicesDropdown = document.getElementById('mobile-services-dropdown');
+
+    servicesMobileButton.addEventListener('click', () => {
+        mobileServicesDropdown.classList.toggle('hidden');
     });
-  });
-  
+});
